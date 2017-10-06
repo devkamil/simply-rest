edefb9e7b59da549d7903fce8b98ffb6aad2f0e1
devkamil <kamilkarwowski.dev@gmail.com>
2017-10-06 23:40:27 +0200

    Add RestController class

---
+++ src/main/java/pl/devkamil/app/AppController.java
@@ -0,0 +1,17 @@
+package pl.devkamil.app;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Autowired
+    public Content content;
+
+    @RequestMapping("/")
+    public String index() {
+        return content.login;
+    }
+}
