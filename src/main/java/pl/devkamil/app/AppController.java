d3629a0c774d78827a0172f9d4ddacf5727dbcf9
devkamil <kamilkarwowski.dev@gmail.com>
2017-10-08 18:55:32 +0200

    Change 'API'p in Controller

--- src/main/java/pl/devkamil/app/AppController.java
+++ src/main/java/pl/devkamil/app/AppController.java
@@ -1,10 +1,11 @@
 package pl.devkamil.app;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/get/user/info/")
 public class AppController {
 
     @Autowired
