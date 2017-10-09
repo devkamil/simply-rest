aa4bc7c685c5e58508cfa6af2f6085ce196bbc33
devkamil <kamilkarwowski.dev@gmail.com>
2017-10-09 03:22:44 +0200

    Add 'User' class

---
+++ src/main/java/pl/devkamil/app/User.java
@@ -0,0 +1,115 @@
+package pl.devkamil.app;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+
+    @JsonProperty("id") private long id;
+    @JsonProperty("login")  private String login;
+    @JsonProperty("name") private String name;
+    @JsonProperty("type") private String type;
+    @JsonProperty("avatar_url") private String avatarUrl;
+    @JsonProperty("created_at") private String createdAt;
+
+    public User() {}
+
+    public User(long id, String login, String name, String type, String avatarUrl, String createdAt) {
+        this.id = id;
+        this.login = login;
+        this.name = name;
+        this.type = type;
+        this.avatarUrl = avatarUrl;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (login != null ? !login.equals(user.login) : user.login != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (type != null ? !type.equals(user.type) : user.type != null) return false;
+        if (avatarUrl != null ? !avatarUrl.equals(user.avatarUrl) : user.avatarUrl != null) return false;
+        return createdAt != null ? createdAt.equals(user.createdAt) : user.createdAt == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (avatarUrl != null ? avatarUrl.hashCode() : 0);
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        return result;
+    }
+
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("{");
+//        sb.append(System.getProperty("line.separator"));
+//        sb.append("\"id\": ").append("\"" + id + "\"" ).append(System.getProperty("line.separator"));
+//        sb.append("\"login\": ").append("\"" + login + "\"").append(System.getProperty("line.separator"));
+//        sb.append("\"name\": ").append("\"" + name + "\"").append(System.getProperty("line.separator"));
+//        sb.append("\"type\": ").append("\"" + type + "\"").append(System.getProperty("line.separator"));
+//        sb.append("\"avatarUrl\": ").append("\"" + avatarUrl + "\"").append(System.getProperty("line.separator"));
+//        sb.append("\"createdAt\": ").append("\"" + createdAt + "\"").append(System.getProperty("line.separator"));
+//        sb.append("}").append(System.getProperty("line.separator"));
+//        return sb.toString();
+//    }
+}
