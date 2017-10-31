4546fa0f918a5768982f28cd835e11ee7adbd39d
devkamil <kamilkarwowski.dev@gmail.com>
2017-10-31 18:25:35 +0100

    Modify 'UserServiceTest', add @AutoConfigureMockMvc

--- src/test/java/pl.devkamil.app/service/UserServiceTest.java
+++ src/test/java/pl.devkamil.app/service/UserServiceTest.java
@@ -1,33 +1,35 @@
 package pl.devkamil.app.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import pl.devkamil.app.exception.CustomException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class UserServiceTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private MockMvc mockMvc;
 
     @Autowired
     private UserService userService;
 
     @Test
     public void requestOctocatShouldContainStringOctocat() throws Exception{
-        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/user/info/octocat",
-                String.class)).contains("octocat");
+        this.mockMvc.perform(get("/user/info/octocat")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("octocat")));
     }
 
     @Test(expected = CustomException.class)
